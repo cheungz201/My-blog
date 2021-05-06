0b56bc8f62ff719b7454b65472a343a96e2f6678
cheungz <zhangzhe201507@foxmail.com>
2021-05-06 13:31:00 +0800

    blog

---
+++ src/test/java/com/my/blog/website/LoginTest.java
@@ -0,0 +1,17 @@
+package com.my.blog.website;
+
+import org.junit.Test;
+
+/**
+ * @Program: my-blog
+ * @Author: Zhang Zhe
+ * @Create: 2021-04-29 12:13
+ * @Version: 1.0.0
+ * @Description:
+ **/
+public class LoginTest {
+    @Test
+    public void test(){
+
+    }
+}
